0f50862912395edc5aa1de951ac7fa50132b6049
cksal0805 <valley@smartstudy.co.kr>
2022-03-08 17:24:06 +0900

    Add husky 적용

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,6 +10,10 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {ignoreRestSiblings: true},
+        ],
       },
     },
   ],
